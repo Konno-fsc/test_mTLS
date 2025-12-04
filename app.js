0d56e8f3a9468e0f39b7367d6c001fa93e8c4b47
Konno-fsc <konno@fscreation.co.jp>
2025-12-04 08:43:04 +0000

    202512041743

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-// server.js の例
 const express = require('express');
 const app = express();
-
-// Azure App Serviceは環境変数PORTを設定します
 const port = process.env.PORT || 8080; 
 
 app.get('/', (req, res) => {
-  res.send('<h1>Node.js App Service is Running!</h1>');
+  // HTMLを返すのはアプリの役割
+  res.send('<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>My First Web App</title></head><body><h1>Hello_World! App.js is Running!</h1></body></html>');
 });
 
 app.listen(port, () => {
